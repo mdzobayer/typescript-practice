66b57b2608949341869efb8b1efd8ac0d63b8961
mdzobayer <mdzobayer@rocketmail.com>
2019-12-10 18:39:14 +0600

    Add lodash library and test a function named reverse

--- npmProject/index.ts
+++ npmProject/index.ts
@@ -1,7 +1,17 @@
 import {Person} from './Person'
+import * as _ from 'lodash';
 
 let foo = new Person();
 foo.firstName = "Test";
 
 foo.lastName = "LastName";
 console.log(foo);
+
+var array = [1,2,3,4,5];
+
+// Type defination or define type
+// cmd: npm install @types/lodash --save-dev
+
+console.log(array);
+_.reverse(array);
+console.log(array);
