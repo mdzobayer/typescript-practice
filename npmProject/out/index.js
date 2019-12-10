3c071c68bd9487c3efd5a2a4bdd22547a8321071
mdzobayer <mdzobayer@rocketmail.com>
2019-12-10 16:56:18 +0600

    Add out directory for output files

---
+++ npmProject/out/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Person_1 = require("./Person");
+var foo = new Person_1.Person();
+foo.firstName = "Test";
+foo.lastName = "LastName";
+console.log(foo);
