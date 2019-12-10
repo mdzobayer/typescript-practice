f195e1987dbca077c5051e024d07b9e939afe7c7
mdzobayer <mdzobayer@rocketmail.com>
2019-12-10 18:37:07 +0600

    Add new js files

--- npmProject/out/index.js
+++ npmProject/out/index.js
@@ -1,7 +1,21 @@
 "use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var Person_1 = require("./Person");
+var _ = __importStar(require("lodash"));
 var foo = new Person_1.Person();
 foo.firstName = "Test";
 foo.lastName = "LastName";
 console.log(foo);
+var array = [1, 2, 3, 4, 5];
+// Type defination or define type
+// cmd: npm install @types/lodash --save-dev
+console.log(array);
+_.reverse(array);
+console.log(array);
