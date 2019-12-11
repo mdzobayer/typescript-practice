58270da96b677f5abd2c4f41fb4c802f241a6b2f
mdzobayer <mdzobayer@rocketmail.com>
2019-12-11 17:04:57 +0600

    A Todo list with raw html and javascript

---
+++ ToDoList/output/script.js
@@ -0,0 +1,21 @@
+
+
+var myList = [];
+
+function PrintList() {
+    var i;
+    var fullList = "";
+
+    for (i = 0; i < myList.length; ++i) {
+        console.log(i + ". " + myList[i]);
+        //console.log("\n");
+        fullList += "<h4>" + (i + 1) + ". " + myList[i] +"</h4>";
+    }
+    document.getElementById("list").innerHTML = fullList;
+}
+
+function AddItem() {
+    var x = document.getElementById("item").value;
+    console.log("New item added: " + x);
+    myList.push(x);
+}
