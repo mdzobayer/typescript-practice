12c2d67ca5f6ff5c0fe8b4b42cf251cef4e17f2c
mdzobayer <mdzobayer@rocketmail.com>
2019-12-11 18:17:27 +0600

    Add typescript generated javascript file

--- ToDoList/output/script.js
+++ ToDoList/output/script.js
@@ -1,19 +1,15 @@
-
-
+"use strict";
 var myList = [];
-
 function PrintList() {
     var i;
     var fullList = "";
-
     for (i = 0; i < myList.length; ++i) {
         console.log(i + ". " + myList[i]);
         //console.log("\n");
-        fullList += "<h4>" + (i + 1) + ". " + myList[i] +"</h4>";
+        fullList += "<h4>" + (i + 1) + ". " + myList[i] + "</h4>";
     }
     document.getElementById("list").innerHTML = fullList;
 }
-
 function AddItem() {
     var x = document.getElementById("item").value;
     if (x != "") {
